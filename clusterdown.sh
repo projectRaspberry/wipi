befae3e5a07973f7e8e37a9a8713b0513309fabc
Sayan Adhikari <sayanadhikari207@gmail.com>
2020-11-15 23:33:49 +0100

    update

---
+++ clusterdown.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo "WiPi Cluster Shutdown"
+echo "====================="
+sudo scontrol update NodeName=node[01-03] state=down reason="power down"
+ssh node01 "sudo halt"
+ssh node02 "sudo halt"
+ssh node03 "sudo halt"
+echo "Nodes disconnected and shutting down"
+echo "Do you want to shutdown master node too?"
+echo "Press 'y' to continue or q to abort"
+count=0
+while : ; do
+read -n 1 k <&1
+if [[ $k = y ]] ; then
+printf "\nShutting down Master Node\n"
+sudo halt
+elif [[ $k = q ]] ; then
+printf "\nShutdown aborted\n" 
+break
+else
+((count=$count+1))
+printf "\nWrong Key Pressed\n"
+echo "Press 'q' to abort"
+fi
+done#!/bin/bash
+sudo scontrol update NodeName=node[01-03] state=resume
+#sudo scontrol update NodeName=node01 state=resume
+sinfo
+echo "Nodes up and running"
---
+++ tempRasp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "WiPi Cluster CPU Temperature Check"
+echo "Temperature Master"
+sudo /opt/vc/bin/vcgencmd measure_temp
+echo "Temperature Node01"
+ssh node01 'sudo /opt/vc/bin/vcgencmd measure_temp'
+
+echo "Temperature Node02"
+ssh node02 'sudo /opt/vc/bin/vcgencmd measure_temp'
+
+echo "Temperature Node03"
+ssh node03 'sudo /opt/vc/bin/vcgencmd measure_temp'
+
+echo "All done"
