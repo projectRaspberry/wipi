d825d26388ccb386ffd2f77dd655819207bbce53
Rinku Mishra <arra4723@gmail.com>
2021-03-01 12:02:00 +0100

    test

--- deployment/master_deployment.sh
+++ deployment/master_deployment.sh
@@ -1,1 +1,18 @@
-dsaasa
+#!/bin/bash
+
+#exit if any command fails
+set -e
+
+if [ `whoami` != "root" ] ; then
+    echo "Rerun as root"
+    exit 1
+fi
+
+apt -y update
+apt -y upgrade
+apt -y install git ntpdate screen vim-nox python3-pip nfs-kernel-server slurm-wlm munge openmpi-bin openmpi-common libopenmpi3 libopenmpi-dev build-essential
+
+git clone --recursive https://github.com/sayanadhikari/wipi.git
+git checkout automated
+chown -R pi:pi /home/pi/pi_cluster
+cd /home/pi/pi_cluster/config
