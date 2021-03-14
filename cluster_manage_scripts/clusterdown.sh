94c357033d23eb058623dc13fb803e666494e585
Sayan Adhikari <sayanadhikari207@gmail.com>
2021-03-14 22:12:27 +0100

    script update

--- cluster_manage_scripts/clusterdown.sh
+++ cluster_manage_scripts/clusterdown.sh
@@ -14,15 +14,11 @@ if [[ $k = y ]] ; then
 printf "\nShutting down Master Node\n"
 sudo halt
 elif [[ $k = q ]] ; then
-printf "\nShutdown aborted\n" 
+printf "\nShutdown aborted\n"
 break
 else
 ((count=$count+1))
 printf "\nWrong Key Pressed\n"
 echo "Press 'q' to abort"
 fi
-done#!/bin/bash
-sudo scontrol update NodeName=node[01-02] state=resume
-#sudo scontrol update NodeName=node01 state=resume
-sinfo
-echo "Nodes up and running"
+done
