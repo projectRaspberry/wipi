5db92a2181cb9919a97c29935d4d905899999aaf
Sayan Adhikari <sayanadhikari207@gmail.com>
2021-03-14 21:49:58 +0100

    ss

--- deployment/password_less_ssh_master.sh
+++ deployment/password_less_ssh_master.sh
@@ -9,4 +9,4 @@ ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
 echo "raspberry">~/passwd.txt
 echo "copying rsa key to the nodes"
 sshpass -f ~/passwd.txt ssh-copy-id pi@node01
-sshpass -f ~/passwd.txt ssh-copy-id pi@node01
+sshpass -f ~/passwd.txt ssh-copy-id pi@node02
