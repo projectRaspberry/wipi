fd1ef190c51f25bb67c41f70c0fab01972f2072e
Sayan Adhikari <sayanadhikari207@gmail.com>
2021-03-14 19:47:35 +0100

    update slurm config script

--- deployment/slurm_config_master.sh
+++ deployment/slurm_config_master.sh
@@ -16,6 +16,20 @@ cp $wipi_repo/deployment/slurm_config/* /etc/slurm-llnl/
 echo "Copying munge key to the shared directory"
 cp /etc/munge/munge.key /shared_dir
 
+echo "Generating RSA Key for authentication"
+ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+
+echo "copying rsa key to the nodes"
+echo -e "raspberry\n" | ssh-copy-id pi@node01
+
+echo "Add a new group named admin and add pi to it"
+groupadd admin
+usermod -a -G admin pi
+
+echo "Adding admin group to sudoes file"
+echo "%admin	ALL=(ALL) ALL" >>/etc/sudoers
+echo "%admin	ALL=(ALL) NOPASSWD: ALL" >>/etc/sudoers
+
 echo "Enable and start SLURM Control Services and munge"
 systemctl enable munge
 systemctl start munge
--- deployment/slurm_config_nodes.sh
+++ deployment/slurm_config_nodes.sh
@@ -16,6 +16,14 @@ cp $wipi_repo/deployment/slurm_config/* /etc/slurm-llnl/
 echo "Copying munge key to the shared directory"
 cp /shared_dir/munge.key /etc/munge/munge.key
 
+echo "Add a new group named admin and add pi to it"
+groupadd admin
+usermod -a -G admin pi
+
+echo "Adding admin group to sudoes file"
+echo "%admin	ALL=(ALL) ALL" >>/etc/sudoers
+echo "%admin	ALL=(ALL) NOPASSWD: ALL" >>/etc/sudoers
+
 echo "Enable and start SLURM Control Services and munge"
 systemctl enable munge
 systemctl start munge
