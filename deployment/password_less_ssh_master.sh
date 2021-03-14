dea65ceff8951cdd55b796fbb941a0a8a85d2648
Rupak Mukherjee <rupakmukherjee01@gmail.com>
2021-03-14 18:27:12 -0400

    Update password_less_ssh_master.sh

--- deployment/password_less_ssh_master.sh
+++ deployment/password_less_ssh_master.sh
@@ -8,5 +8,5 @@ ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
 
 echo "raspberry">~/passwd.txt
 echo "copying rsa key to the nodes"
-sshpass -p raspberry ssh -o StrictHostKeyChecking=no pi@node01
-sshpass -p raspberry ssh -o StrictHostKeyChecking=no pi@node02
+sshpass -p raspberry ssh-copy-id pi@node01
+sshpass -p raspberry ssh-copy-id pi@node02
